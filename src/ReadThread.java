97167a17031d6ff63c597d31d9c2bd3b33ee035e
SadiaDiba <diba.sadia22@gmail.com>
2017-03-08 00:01:22 +0600

    Commented ReadThread to test

--- src/ReadThread.java
+++ src/ReadThread.java
@@ -1,33 +1,33 @@
-import java.io.ObjectInputStream;
-
-public class ReadThread implements Runnable {
-	private Thread thr;
-	ObjectInputStream ois;
-	ChatWindow chatWindow;
-
-	public ReadThread(ObjectInputStream ois,ChatWindow chatWindow) {
-		this.ois = ois;
-		this.thr = new Thread(this);
-		this.chatWindow=chatWindow;
-		thr.start();
-
-	}
-
-	public void run() {
-		try {
-
-			while (true) {
-				String senderName = (String) ois.readObject();
-				String message=(String)ois.readObject();
-				if (message != null) {			
-					// sending the text to chatwindow
-					chatWindow.setText(senderName+": "+message);
-				}
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-	}
-}
-
+//import java.io.ObjectInputStream;
+//
+//public class ReadThread implements Runnable {
+//	private Thread thr;
+//	ObjectInputStream ois;
+//	ChatWindow chatWindow;
+//
+//	public ReadThread(ObjectInputStream ois,ChatWindow chatWindow) {
+//		this.ois = ois;
+//		this.thr = new Thread(this);
+//		this.chatWindow=chatWindow;
+//		thr.start();
+//
+//	}
+//
+//	public void run() {
+//		try {
+//
+//			while (true) {
+//				String senderName = (String) ois.readObject();
+//				String message=(String)ois.readObject();
+//				if (message != null) {			
+//					// sending the text to chatwindow
+//					chatWindow.setText(senderName+": "+message);
+//				}
+//			}
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
+//
+//	}
+//}
+//
